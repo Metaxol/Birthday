01d71633c0dc81b6a3b5e6c9ec06977bba16f1f4
Metaxol <45857811+Metaxol@users.noreply.github.com>
2019-03-14 20:12:35 +0100

    Player
    Worked on Player Anims and Movement, gotta fix horizontal movement bug

---
+++ BirthdayGame/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D PlayerRigidbody2D;
+    private bool PlayerCanMove = true;
+
+    private void Move_Player(float MoveSpeed) //This function is for moving the player.
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if (PlayerCanMove)
+        {
+            PlayerRigidbody2D.velocity = new Vector3(x * MoveSpeed * Time.deltaTime, y * MoveSpeed * Time.deltaTime, 0);
+        }
+    }
+   
+    private void FixedUpdate()
+    {
+        Move_Player(100f); //Parameter decides MoveSpeed. 
+    }
+    
+    private void Awake()
+    {
+        PlayerRigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+}
