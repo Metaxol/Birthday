30484fed4e956374fb147aec1ee0d8f54aa6f676
Metaxol <45857811+Metaxol@users.noreply.github.com>
2019-04-30 22:29:06 +0200

    continue
    continue with making the intro and shit

---
+++ BirthdayGame/Assets/StartGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+    public bool forStartMenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (forStartMenu == true)
+            {
+                SceneManager.LoadScene("Level_Scene");
+            }           
+        }
+    }
+}
